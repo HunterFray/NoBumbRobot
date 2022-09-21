b017ac10db47a32fcd835bcc35bc9f48b82ea88e
HunterFray <114106016+HunterFray@users.noreply.github.com>
2022-09-21 21:19:46 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from machine import Pin, I2C
+from machine import Pin, I2C, PWM
 import utime
 from sh1106 import SH1106_I2C
 from hcsr04 import HCSR04
@@ -8,21 +8,33 @@ oled = SH1106_I2C(128, 64, i2c)
 sensor = HCSR04(trigger_pin=6, echo_pin=7,echo_timeout_us=1000000)
 distance = sensor.distance_cm()
 min_dist = 30
+
 def dist():
     distance = sensor.distance_cm()
     print(distance)
     utime.sleep(1)
-    oled.fill(0)
-    oled.text("Distance: ", 10, 20)
-    oled.text(str(distance),10,30)
-    oled.show()
+    
+    if sensor.distance_cm() >= min_dist:
+        print("no obstacles")
+        oled.fill(0)
+        oled.text("Distance: ", 10, 20)
+        oled.text(str(distance), 10,30)
+        oled.text("Path is", 10, 40)
+        oled.text("clear", 10, 50)
+        oled.show()
+    else:
+        print("collision likely")
+        oled.fill(0)
+        oled.text("Distance: ", 10, 20)
+        oled.text(str(distance),10,30)
+        oled.text("Collision", 10,40)
+        oled.text("Likely", 10,50)
+        oled.show()
+    
 
 while True:
     dist()
-    if sensor.distance_cm() >= min_dist:
-        print("no obstacles")
-    else:
-        print("collision likely")
+    
         
     
 
