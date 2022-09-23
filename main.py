6295327ff703bf340039cf049cf2547b47c9e71e
HunterFray <114106016+HunterFray@users.noreply.github.com>
2022-09-23 15:02:23 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,14 +2,33 @@ from machine import Pin, I2C, PWM
 import utime
 from sh1106 import SH1106_I2C
 from hcsr04 import HCSR04
+import math
+from motor import Motor, pico_motor_shim
+from pimoroni import NORMAL_DIR, REVERSED_DIR
 
-i2c = I2C(0, scl=Pin(1), sda=Pin(0), freq=400000)
+
+i2c = I2C(0, scl=Pin(17), sda=Pin(16), freq=400000)
 oled = SH1106_I2C(128, 64, i2c)
-sensor = HCSR04(trigger_pin=6, echo_pin=7,echo_timeout_us=1000000)
+oled.rotate(1)
+sensor = HCSR04(trigger_pin=14, echo_pin=15,echo_timeout_us=1000000)
 distance = sensor.distance_cm()
 min_dist = 30
 
-def dist():
+SPEED_SCALE = 5.4               # The scaling to apply to each motor's speed to match its real-world speed
+DRIVING_SPEED = SPEED_SCALE     # The speed to drive the wheels at, from 0.0 to SPEED_SCALE
+left = Motor(pico_motor_shim.MOTOR_1, direction=NORMAL_DIR, speed_scale=SPEED_SCALE)
+right = Motor(pico_motor_shim.MOTOR_2, direction=REVERSED_DIR, speed_scale=SPEED_SCALE)
+
+
+def backward(speed=DRIVING_SPEED):
+    left.speed(-speed)
+    right.speed(-speed)
+
+def forward(speed=DRIVING_SPEED):
+            left.speed(speed)
+            right.speed(speed)
+            
+def robot():
     distance = sensor.distance_cm()
     print(distance)
     utime.sleep(1)
@@ -22,6 +41,7 @@ def dist():
         oled.text("Path is", 10, 40)
         oled.text("clear", 10, 50)
         oled.show()
+        forward()
     else:
         print("collision likely")
         oled.fill(0)
@@ -33,7 +53,7 @@ def dist():
     
 
 while True:
-    dist()
+    robot()
     
         
     
