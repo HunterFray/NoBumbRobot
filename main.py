6c104625e4c20b76d8c1d218056ae3431bf564e2
HunterFray <114106016+HunterFray@users.noreply.github.com>
2022-09-27 16:31:32 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,67 +1,56 @@
-from machine import Pin, I2C, PWM
+from machine import Pin, PWM, I2C
 import utime
 from sh1106 import SH1106_I2C
 from hcsr04 import HCSR04
+from motor_driver import *
 import math
-from motor import Motor, pico_motor_shim
-from pimoroni import NORMAL_DIR, REVERSED_DIR
 
-
-i2c = I2C(0, scl=Pin(17), sda=Pin(16), freq=400000)
-oled = SH1106_I2C(128, 64, i2c)
-oled.rotate(1)
-sensor = HCSR04(trigger_pin=14, echo_pin=15,echo_timeout_us=1000000)
+#i2c = I2C(0, sda=Pin(6), scl=Pin(7), freq=400000)
+#oled = SH1106_I2C(128, 64, i2c)
+#oled.rotate
+sensor = HCSR04(trigger_pin=4, echo_pin=5, echo_timeout_us=1000000)
 distance = sensor.distance_cm()
-min_dist = 30
+min_dist = 20
+motor = motor_driver(0,1,2,3) #Set pins for motor A and B  M1A=Pin0,M1B=Pin1,M2A=Pin3,M2B=Pin4
 
-SPEED_SCALE = 5.4               # The scaling to apply to each motor's speed to match its real-world speed
-DRIVING_SPEED = SPEED_SCALE     # The speed to drive the wheels at, from 0.0 to SPEED_SCALE
-left = Motor(pico_motor_shim.MOTOR_1, direction=NORMAL_DIR, speed_scale=SPEED_SCALE)
-right = Motor(pico_motor_shim.MOTOR_2, direction=REVERSED_DIR, speed_scale=SPEED_SCALE)
+def forward():
+    motor.speed(75,75)
+    
+def reverse():
+    motor.speed(-50,-50)
+    
+def turn_left():
+    motor.speed(0,50)
+    
+def allstop():
+    motor.brake()
 
+def stop_turn():
+    motor.brake()
+    utime.sleep(1)
+    motor.speed(0,50)
 
-def backward(speed=DRIVING_SPEED):
-    left.speed(-speed)
-    right.speed(-speed)
-
-def forward(speed=DRIVING_SPEED):
-            left.speed(speed)
-            right.speed(speed)
-            
 def robot():
     distance = sensor.distance_cm()
     print(distance)
-    utime.sleep(1)
     
-    if sensor.distance_cm() >= min_dist:
-        print("no obstacles")
-        oled.fill(0)
-        oled.text("Distance: ", 10, 20)
-        oled.text(str(distance), 10,30)
-        oled.text("Path is", 10, 40)
-        oled.text("clear", 10, 50)
-        oled.show()
+    if distance >= min_dist:
+        print("Path is clear")
+        #oled.fill(0)
+        #oled.text("Path is", 10,10)
+        #oled.text("Clear",20,20)
+        #oled.show
         forward()
     else:
-        print("collision likely")
-        oled.fill(0)
-        oled.text("Distance: ", 10, 20)
-        oled.text(str(distance),10,30)
-        oled.text("Collision", 10,40)
-        oled.text("Likely", 10,50)
-        oled.show()
-    
-
-while True:
-    robot()
-    
+        print("Collision likely")
+        #oled.fill(0)
+        #oled.text("Collision", 10,10)
+        #oled.text("Likely", 20,20)
+        #oled.show()
+        #allstop()
+        stop_turn()
+        utime.sleep(2)
         
-    
-
-
-
-   
-
-
-
-   
+while True:            
+    robot()
+        
